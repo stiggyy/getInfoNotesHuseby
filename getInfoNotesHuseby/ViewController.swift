e2e9968083028e5da5d615cbfea07ae46ed84c2d
c huseby <catherine_huseby@outlook.com>
2023-08-30 09:27:40 -0500

    Initial Commit

---
+++ getInfoNotesHuseby/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  getInfoNotesHuseby
+//
+//  Created by CATHERINE HUSEBY on 8/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameOutlet: UILabel!
+    
+    @IBOutlet weak var textOutlet: UITextField!
+    override func viewDidLoad() {
+        
+    
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func submitAction(_ sender: Any) {
+        //getting text from field
+        var name = textOutlet.text
+        if let x = name{
+            
+            nameOutlet.text = "Hello \(x)!"
+            
+        }
+        
+        
+    }
+    
+
+}
+
